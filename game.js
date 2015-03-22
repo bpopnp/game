eb53e0f3bb927a7bdd4f89d517e6e2168fd267d9
pdlan <pengdinglan@gmail.com>
2015-03-22 20:27:56 +0800

    3

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
 var utils = {};
-document.write('<script src="https://raw.githubusercontent.com/bpopnp/game/master/jquery-1.11.1.min.js" type="text/javascript"></script>');
-document.write('<script src="https://raw.githubusercontent.com/bpopnp/game/master/utils.js" type="text/javascript"></script>');
-document.write('<script src="https://raw.githubusercontent.com/bpopnp/game/master/snake.js" type="text/javascript"></script>');
+document.write('<script src="https://rawgit.com/bpopnp/game/master/jquery-1.11.1.min.js" type="text/javascript"></script>');
+document.write('<script src="https://rawgit.com/bpopnp/game/master/utils.js" type="text/javascript"></script>');
+document.write('<script src="https://rawgit.com/bpopnp/game/master/snake.js" type="text/javascript"></script>');
